ad96a1fa17d9b9052220b2ecb7cdd7929bb257c1
chmg2025 <chmg2025@qq.com>
2025-04-09 22:14:27 +0800

    Update mdsp.js

--- mdsp.js
+++ mdsp.js
@@ -4,7 +4,6 @@
  #!author=小白
  #!date=2025-04-09
 
-
  [Script]
  # 会员
  http-response https://d1skbu98kuldnf.cloudfront.net/api/app/user/info script-path=https://raw.githubusercontent.com/chmg2025/script/refs/heads/main/mdsp.js, requires-body=true, timeout=60, tag=会员
