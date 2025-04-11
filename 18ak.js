4e80306be9ca66ed746554cce5cbe9940ca77c1f
chmg2025 <chmg2025@qq.com>
2025-04-11 22:30:52 +0800

    Update 18ak.js

--- 18ak.js
+++ 18ak.js
@@ -7,6 +7,8 @@
  http-response https://18akmanhua.com/api/memberDetails script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/18ak.js, requires-body=true, timeout=60, tag=会员
  http-response ^https:\/\/18akmanhua\.com\/api\/videoDetails\?id=\d+$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/18ak.js, requires-body=true, timeout=60, tag=视频
  http-response https://18akmanhua.com/api/playNewVideos script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/18ak.js, requires-body=true, timeout=60, tag=播放
+ http-response ^https:\/\/18akmanhua\.com\/api\/chaptersDetails\?chapter_id=\d+$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/18ak.js, requires-body=true, timeout=60, tag=漫画
+ http-response ^https:\/\/18akmanhua\.com\/api\/chaptersDirectories\?.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/18ak.js, requires-body=true, timeout=60, tag=漫画列表
 
  [MITM]
  hostname = 18akmanhua.com
