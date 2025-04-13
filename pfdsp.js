e469303cc333d1c22cd7cbc2d20fa5a4f3501fdf
chmg2025 <chmg2025@qq.com>
2025-04-13 15:37:48 +0800

    Update pfdsp.js

--- pfdsp.js
+++ pfdsp.js
@@ -4,9 +4,9 @@
  #!author=小白
  #!date=2025-04-13
  [Script]
- http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/ping/domain/h5 script-path=https://vip.chmg2025.ip-ddns.com/pfdsp.js, requires-body=true, timeout=60, tag=广告
- http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/mine/info script-path=https://vip.chmg2025.ip-ddns.com/pfdsp.js, requires-body=true, timeout=60, tag=会员
- http-response ^https:\/\/(jnhb8y79\.com|d5flxjpige3b9\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://vip.chmg2025.ip-ddns.com/pfdsp.js, requires-body=true, timeout=60, tag=视频
+ http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/ping/domain/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=广告
+ http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/mine/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=会员
+ http-response ^https:\/\/(jnhb8y79\.com|d5flxjpige3b9\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/jst/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=视频
 
 
  [MITM]
