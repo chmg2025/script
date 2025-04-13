1e4435176fe4674bdb08e208049a88da5971a357
chmg2025 <chmg2025@qq.com>
2025-04-13 15:35:29 +0800

    Create pfdsp.js

---
+++ pfdsp.js
@@ -0,0 +1,14 @@
+/***********************************
+ #!name=泡芙短视频
+ #!desc=去广告全解锁(支持Loon和圈X)
+ #!author=小白
+ #!date=2025-04-13
+ [Script]
+ http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/ping/domain/h5 script-path=https://vip.chmg2025.ip-ddns.com/pfdsp.js, requires-body=true, timeout=60, tag=广告
+ http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/mine/info script-path=https://vip.chmg2025.ip-ddns.com/pfdsp.js, requires-body=true, timeout=60, tag=会员
+ http-response ^https:\/\/(jnhb8y79\.com|d5flxjpige3b9\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://vip.chmg2025.ip-ddns.com/pfdsp.js, requires-body=true, timeout=60, tag=视频
+
+
+ [MITM]
+ hostname = jnhb8y79.com, *.cloudfront.net
+ ***********************************/
