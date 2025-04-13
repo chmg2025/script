20bec0147a57aa59e686a944b20d3c207fb3b0a8
chmg2025 <chmg2025@qq.com>
2025-04-13 22:25:59 +0800

    Update mdsp.js

--- mdsp.js
+++ mdsp.js
@@ -1,8 +1,8 @@
 /***********************************
- #!name=麻豆视频
+ #!name=麻豆视频 + 快手萝莉
  #!desc=去广告解锁视频(支持LOON和圈X)
  #!author=小白
- #!date=2025-04-10
+ #!date=2025-04-13
  #!openUrl= https://d1skbu98kuldnf.cloudfront.net/home
 
  [Script]
