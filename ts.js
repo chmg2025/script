b39764c1ae91cefac2c08ab5efe9d071a7c7cc50
chmg2025 <chmg2025@qq.com>
2025-04-16 21:47:34 +0800

    Update ts.js

--- ts.js
+++ ts.js
@@ -2,7 +2,7 @@
 #!name=通杀
 #!desc=解锁VIP视频
 #!author=小白
-#!date=2024-11-16
+#!date=2025-04-16
 
 [Script]
 http-response ^https:\/\/[^\/]*\.(.*\.cn)(\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8)(\?.*)?$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/ts.js, requires-body=true, timeout=60, tag=通杀
