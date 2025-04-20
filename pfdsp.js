fdcb0411fd6e6bb92d4e111c9aed64bfad850419
chmg2025 <chmg2025@qq.com>
2025-04-20 16:40:31 +0800

    Update pfdsp.js

--- pfdsp.js
+++ pfdsp.js
@@ -2,11 +2,11 @@
  #!name=泡芙短视频
  #!desc=去广告全解锁(支持Loon和圈X)
  #!author=小白
- #!date=2025-04-13
+ #!date=2025-04-20
  [Script]
- http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/ping/domain/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=广告
- http-response https://(jnhb8y79.com|d5flxjpige3b9.cloudfront.net)/api/app/mine/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=会员
- http-response ^https:\/\/(jnhb8y79\.com|d5flxjpige3b9\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=视频
+ http-response https://(jnhb8y79.com|(d5flxjpige3b9|d25jkl3n16fhe).cloudfront.net)/api/app/ping/domain/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=广告
+ http-response https://(jnhb8y79.com|(d5flxjpige3b9|d25jkl3n16fhe).cloudfront.net)/api/app/mine/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=会员
+ http-response ^https:\/\/(jnhb8y79\.com|(d5flxjpige3b9|d25jkl3n16fhe)\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/pfdsp.js, requires-body=true, timeout=60, tag=视频
 
 
  [MITM]
