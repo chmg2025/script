58d43c7b3685b74f164c1ad9f5021adce64edc5a
chmg2025 <chmg2025@qq.com>
2025-05-20 20:57:36 +0800

    Update mr.js

--- mr.js
+++ mr.js
@@ -1,1 +1,19 @@
+/***********************************
+ #!name=Mr.好色
+ #!desc=去广告解锁视频(支持Quantumult X 和 LOON)
+ #!author=小白
+ #!date=2025-05-20
 
+
+ [Script]
+ # 会员
+ http-response https://(3e8qs.com|(d2m18pn7x0p3im|d27nudycn51u9q).cloudfront.net)/api/app/mine/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=会员
+ # 视频
+ http-response ^https:\/\/(3e8qs\.com|(d2m18pn7x0p3im|d27nudycn51u9q)\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=视频
+ # 广告
+ http-response https://(3e8qs.com|(d2m18pn7x0p3im|d27nudycn51u9q).cloudfront.net)/api/app/ping/domain/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=广告
+
+
+ [MITM]
+ hostname = 3e8qs.com, *.cloudfront.net
+ ***********************************/
