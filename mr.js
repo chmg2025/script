881351dc39a09da024d8b8c385e2cd36b924f488
11 <11@qq.com>
2025-05-23 20:32:10 +0800

    更新域名匹配正则

--- mr.js
+++ mr.js
@@ -7,11 +7,11 @@
 
  [Script]
  # 会员
- http-response https:\/\/(3e8qs.com|[^\/]+\.cloudfront.net)\/api\/app\/mine\/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=会员
+ http-response https://(3e8qs.com|[^\/]+\.cloudfront.net)/api/app/mine/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=会员
  # 视频
  http-response ^https:\/\/(3e8qs\.com|[^\/]+\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=视频
  # 广告
- http-response https:\/\/(3e8qs.com|[^\/]+\.cloudfront.net)\/api\/app\/ping\/domain\/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=广告
+ http-response https://(3e8qs.com|[^\/]+\.cloudfront.net)/api/app/ping/domain/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=广告
 
 
  [MITM]
