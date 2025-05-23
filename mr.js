4a19d06c961a35ded3598a6ef6b37e59a58ef5d0
11 <11@qq.com>
2025-05-23 20:17:03 +0800

    更新域名

--- mr.js
+++ mr.js
@@ -7,11 +7,11 @@
 
  [Script]
  # 会员
- http-response https://(3e8qs.com|(d2m18pn7x0p3im|d27nudycn51u9q).cloudfront.net)/api/app/mine/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=会员
+ http-response https://(3e8qs.com|(d2m18pn7x0p3im|d27nudycn51u9q|d2wexzpo1hxhi0).cloudfront.net)/api/app/mine/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=会员
  # 视频
- http-response ^https:\/\/(3e8qs\.com|(d2m18pn7x0p3im|d27nudycn51u9q)\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=视频
+ http-response ^https:\/\/(3e8qs\.com|(d2m18pn7x0p3im|d27nudycn51u9q|d2wexzpo1hxhi0)\.cloudfront\.net)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=视频
  # 广告
- http-response https://(3e8qs.com|(d2m18pn7x0p3im|d27nudycn51u9q).cloudfront.net)/api/app/ping/domain/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=广告
+ http-response https://(3e8qs.com|(d2m18pn7x0p3im|d27nudycn51u9q|d2wexzpo1hxhi0).cloudfront.net)/api/app/ping/domain/h5 script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=广告
 
 
  [MITM]
