c77c3fbc57438abece06446d6156635fc8b4693d
chmg2025 <chmg2025@protonmail.com>
2025-05-29 21:53:27 +0800

    TsBox解锁VIP

--- tsbox.js
+++ tsbox.js
@@ -3,6 +3,7 @@
  #!desc=解锁VIP(支持Quantumult X 和 Loon)
  #!author=小白
  #!date=2025-05-29
+ #!openUrl = https://tsbox0.top
  [Script]
  http-response ^https:\/\/[^\/]+\.tsboxhost\.top\/api\/token_login\?.* script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/tsbox.js, requires-body=true, timeout=60, tag=会员
 
