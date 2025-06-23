ad929e6cd4eebf007ea67e830bf49632d7236479
chmg2025 <chmg2025@protonmail.com>
2025-06-23 22:45:04 +0800

    测试

---
+++ yqx.js
@@ -0,0 +1,19 @@
+/****************************
+#!name = 亿启秀
+#!desc = 测试
+#!author = 小白
+#!date = 2025-06-23
+
+
+[Script]
+http-response https://www.xixixtv.cc/hapi/liveDevice/index requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/chmg2025/script/refs/heads/main/yqx.js, tag=亿启秀
+
+[Mitm]
+hostname = www.xixixtv.cc
+****************************/
+
+let body = JSON.parse($response.body)
+body.forEach(item => {
+    item.isVip = 1;
+});
+$done({body:JSON.stringify(body)})
