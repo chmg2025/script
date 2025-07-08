cc8670f24855f30daec95fdd01f534aa2c0d11e6
chmg2025 <chmg2025@qq.com>
2025-07-08 21:15:05 +0800

    Create zmq.js

---
+++ zmq.js
@@ -0,0 +1,15 @@
+/***********************************
+ #!name=字母圈
+ #!desc=解锁VIP及金币视频
+ #!author=小白
+ #!date=2025-07-08
+ #!openurl=www.zimuquan.top
+
+ [Script]
+ # 字母圈
+ http-response ^https?:\/\/[^\/]+\/index\.php\/vod\/play\/id\/\d+\/sid\/\d+\/nid\/\d+\.html script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/zmq.js, requires-body=true, timeout=60, tag=字母圈
+
+
+ [MITM]
+ hostname = www.zimuquan.top, zimuquan.top
+ ***********************************/
