cb17a1f43ed78cb4d23fb810f3a1e6dd34e0700e
chmg2025 <chmg2025@qq.com>
2025-07-19 20:33:53 +0800

    Update ydm.js

--- ydm.js
+++ ydm.js
@@ -2,7 +2,7 @@
 #!name= YDM系列通杀
 #!desc=去广告解锁视频(支持LOON和圈X)
 #!author=小白
-#!date=2025-07-11
+#!date=2025-07-19
 解锁站点如下: 
 DeepSeek: https://d2x03a61ogs2x5.cloudfront.net
 YDM:  https://dd38dkt7dfvyr.cloudfront.net
@@ -14,13 +14,14 @@ UUSP: https://d11rd8m1pg017m.cloudfront.net
 MGQ:  https://aw0702.x5t5d5a4c.work/
 BZSP: https://fyb1.looufegitw.work
 MXBC: https://jsj5191.h6b7f5t4n.work/
+GGS:  https://ggs1.gtlxb.work
 [Rewrite]
 # 广告
-^https:\/\/[^\/]+\.(cloudfront\.net|(pxyzjmspfl|rnuozrryfq|looufegitw|h6b7f5t4n|x5t5d5a4c|djfrhyhtuo)\.work)\/api\/(sys\/partner\/list|activity\/indexActs|sys\/advertisement\/list|sys\/getImgAndVideoCdnList|aibox\/entranceConfig) - reject-200
+^https:\/\/[^\/]+\.(cloudfront\.net|(pxyzjmspfl|rnuozrryfq|looufegitw|h6b7f5t4n|x5t5d5a4c|djfrhyhtuo|gtlxb)\.work)\/api\/(sys\/partner\/list|activity\/indexActs|sys\/advertisement\/list|sys\/getImgAndVideoCdnList|aibox\/entranceConfig) - reject-200
 
 [Script]
 # 会员
-http-response ^https:\/\/[^\/]+\.(cloudfront\.net|(pxyzjmspfl|rnuozrryfq|looufegitw|h6b7f5t4n|x5t5d5a4c|djfrhyhtuo)\.work)\/api\/user\/base\/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/ydm.js, requires-body=true, timeout=60, tag=会员
+http-response ^https:\/\/[^\/]+\.(cloudfront\.net|(pxyzjmspfl|rnuozrryfq|looufegitw|h6b7f5t4n|x5t5d5a4c|djfrhyhtuo|gtlxb)\.work)\/api\/user\/base\/info script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/ydm.js, requires-body=true, timeout=60, tag=会员
 # 视频
 http-response ^https:\/\/[^\/]+\.(cloudfront\.net|(pxyzjmspfl|rnuozrryfq|looufegitw|h6b7f5t4n|x5t5d5a4c|djfrhyhtuo)\.work)\/api\/video\/getVideoById script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/ydm.js, requires-body=true, timeout=60, tag=视频
 # 社区
@@ -28,5 +29,5 @@ http-response ^https:\/\/[^\/]+\.(cloudfront\.net|(pxyzjmspfl|rnuozrryfq|looufeg
 
 
 [MITM]
-hostname = *.cloudfront.net, *.pxyzjmspfl.work, *.rnuozrryfq.work, *.looufegitw.work, *.h6b7f5t4n.work, *.x5t5d5a4c.work, *.djfrhyhtuo.work
+hostname = *.cloudfront.net, *.pxyzjmspfl.work, *.rnuozrryfq.work, *.looufegitw.work, *.h6b7f5t4n.work, *.x5t5d5a4c.work, *.djfrhyhtuo.work, *.gtlxb.work
 ***********************************/
