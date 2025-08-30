af0008ac7ca4c96fd2c09cbec4a925c80b076ea5
chmg2025 <chmg2025@qq.com>
2025-08-30 23:18:39 +0800

    Update 65kan.js

--- 65kan.js
+++ 65kan.js
@@ -2,23 +2,22 @@
 /***********************************
  #!name=65看
  #!desc=去广告解锁视频
- #!author=小白
- #!date=2025-04-16
+ #!date=2025-08-30
 
 
  [Rewrite]
- ^https://hm-img\.xuezhumall\.com/videos/(.+)/preview\.m3u8$ header https://hm-img.xuezhumall.com/videos/$1/index.m3u8
- ^https://hm-img\.xuezhumall\.com/videos/(.+)/preview\.m3u8$ url 302 https://hm-img.xuezhumall.com/videos/$1/index.m3u8
+ ^https://hm-img\.(xuezhumall|gycss\.(com|cn)/videos/(.+)/preview\.m3u8$ header https://hm-img.xuezhumall.com/videos/$1/index.m3u8
+ ^https://hm-img\.(xuezhumall|gycss\.(com|cn)/videos/(.+)/preview\.m3u8$ url 302 https://hm-img.xuezhumall.com/videos/$1/index.m3u8
 
  [Script]
  # 会员
- http-response ^https://sm-api\.xuezhumall\.com/api/users/profile\?* script-path=https://raw.githubusercontent.com/chmg2025/script/refs/heads/main/65kan.js, requires-body=true, timeout=60, tag=会员
+ http-response ^https://sm-api\.(xuezhumall|gycss\.(com|cn)/api/users/profile\?* script-path=https://raw.githubusercontent.com/chmg2025/script/refs/heads/main/65kan.js, requires-body=true, timeout=60, tag=会员
  # 广告
- http-response ^https://sm-api\.xuezhumall\.com/api/common_ads\?* script-path=https://raw.githubusercontent.com/chmg2025/script/refs/heads/main/65kan.js, requires-body=true, timeout=60, tag=广告
+ http-response ^https://sm-api\.(xuezhumall|gycss\.(com|cn)/api/common_ads\?* script-path=https://raw.githubusercontent.com/chmg2025/script/refs/heads/main/65kan.js, requires-body=true, timeout=60, tag=广告
 
 
  [MITM]
- hostname = *.xuezhumall.com, *.xyz
+ hostname = *.xuezhumall.com, gycss.cn
  ***********************************/
 
 let $ = new Env('65看片')
