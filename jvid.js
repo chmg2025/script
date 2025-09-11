ea985aa085e69708fc033328ff7113a34773f42a
chmg2025 <chmg2025@qq.com>
2025-09-11 19:55:01 +0800

    Update jvid.js

--- jvid.js
+++ jvid.js
@@ -1,7 +1,7 @@
 /***********************************
 #!name=JVID大陆版 + XVIDEOS免费版 
-#!desc=去广告解锁会员及视频(XVIDEOS免费版仅去广告,金币视频无解)
-#!date=2025-09-10
+#!desc=去广告解锁会员及视频(金币视频无解)
+#!date=2025-09-11
 
 [Script]
 http-response https://((kmfhb0n|l5jzr).cdv6x4x6.cc|k4.uywhqqxp.cc|p5e.x5vuztxr.cc)/front/system/(bannersetting/userGetBannerSet|banner/bannerListByMAcct|datadictionary/getDicByKeys) script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/jvid.js, requires-body=true, timeout=60, tag=广告
