cdd6954c2853ced3e79b4af9f251c98eade0d8ef
chmg2025 <chmg2025@qq.com>
2025-10-27 21:24:58 +0800

    Update ytk.js

--- ytk.js
+++ ytk.js
@@ -3,7 +3,7 @@
  #!desc = 解锁收费视频
  #!date = 2025-10-27
  [Script]
- http-response ^https?:\/\/yitongkan\.club\/play-[a-fA-F0-9]{32}\.html script-path=https://raw.githubusercontent.com/chmg2025/script/refs/heads/main/ytk.js, requires-body=true, timeout=60, tag=一同看
+ http-response ^https?:\/\/yitongkan\.club\/play-[a-fA-F0-9]{32}\.html script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/ytk.js, requires-body=true, timeout=60, tag=一同看
 
  [MITM]
  hostname = yitongkan.club
