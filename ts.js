9ecffed28bb3ff26c85c3587bb966f3b554a5875
chmg2025 <chmg2025@qq.com>
2025-11-15 13:50:11 +0800

    更新 ts.js

--- ts.js
+++ ts.js
@@ -1,12 +1,11 @@
 /***********************************
 #!name=通杀
-#!desc=解锁VIP视频
-#!author=小白
+#!desc=91系列通杀,仅解锁所有完整视频,并未解锁在线观看,需要配合带探嗅浏览器使用(由于匹配域名很很广,不使用建议关闭脚本开关)
 #!date=2025-04-16
 
 [Script]
 http-response ^https:\/\/[^\/]*\.(.*\.cn)(\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8)(\?.*)?$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/ts.js, requires-body=true, timeout=60, tag=通杀
 
 [MITM]
-hostname = *.ivldhj.cn, *.cn
+hostname = *.cn
 ***********************************/
