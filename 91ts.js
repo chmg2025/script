2549d7da759f723dd1739ce80a7cbd72f3457b1f
chmg2025 <chmg2025@qq.com>
2025-12-07 22:02:06 +0800

    Update 91ts.js

--- 91ts.js
+++ 91ts.js
@@ -3,7 +3,6 @@
  #!desc=去广告全解锁[暂时解锁海角社区和微性，后续添加]
  #!date=2025-12-07
 
-
  [Script]
  # 广告
  http-response ^https:\/\/(api2\.upwqjcun\.cc|bak\.haijiaoc\.com|api2\.xivjndn\.xyz|wapi\.wxav\.cc)\/(api|api\.php\/api)\/(home\/getconfig|(tabnew|novel|porngame)\/.*|community\/topics|contents\/list_contents) script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91ts.js, requires-body=true, timeout=60, tag=广告
