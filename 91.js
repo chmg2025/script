9ecb0626d1343dd0dd59a44c0baa424012fe2245
chmg2025 <chmg2025@qq.com>
2026-01-02 21:57:34 +0800

    Create 91.js

---
+++ 91.js
@@ -0,0 +1,12 @@
+/***********************************
+ #!name=91短视频
+ #!desc=去广告全解锁
+ #!date=2026-01-02
+
+ [Script]
+ # 91短视频 
+ http-response ^https:\/\/([a-z0-9-]+\.)?evyccjk\.cc\/pwa\.php script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91.js, requires-body=true, timeout=60, tag=91短视频
+
+ [MITM]
+ hostname = *.evyccjk.cc
+ ***********************************/
