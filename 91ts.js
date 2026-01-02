4480d8f167e278da17f83a53e4890bd7eb5bce4b
chmg2025 <chmg2025@qq.com>
2026-01-02 22:19:35 +0800

    Update 91ts.js

--- 91ts.js
+++ 91ts.js
@@ -1,7 +1,7 @@
 /***********************************
  #!name=91系列通杀
  #!desc=去广告全解锁(解锁列表:【海角社区, 微性, 小红书, 潘多拉】)
- #!date=2026-01-01
+ #!date=2026-01-02
 
 
  [Script]
@@ -10,7 +10,7 @@
  # 会员
  http-response https://[^\/]+\.(cc|xyz|com|net)/(api|api.php/api)/(users/base_info|user/userinfo) script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91ts.js, requires-body=true, timeout=60, tag=会员
  # 视频
- http-response https://[^\/]+\.(cc|xyz|com|net)/(api|api.php/api)/(mv/(detail|getDetail|list_construct)|[^\/]+/(detail|get_detail)) script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91ts.js, requires-body=true, timeout=60, tag=视频
+ http-response https://[^\/]+\.(cc|xyz|com|net)/(api|api.php/api)/(mv/(detail|getDetail|list_construct)|[^\/]+/(detail|get_detail)|vlog\/list_sort) script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91ts.js, requires-body=true, timeout=60, tag=视频
  # 社区
  http-response https://[^\/]+\.(cc|xyz|com|net)/(api|api.php/api)/community/post_detail script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91ts.js, requires-body=true, timeout=60, tag=社区
  # 列表
