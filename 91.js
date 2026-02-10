cc3724dc325bcc88f7ade01f352dea78dba68669
chmg2025 <chmg2025@qq.com>
2026-02-10 18:24:01 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -1,12 +1,12 @@
 /***********************************
  #!name=91短视频
  #!desc=去广告全解锁
- #!date=2026-01-23
+ #!date=2026-02-10
 
  [Script]
  # 91短视频 
- http-response ^https:\/\/([a-z0-9-]+\.)?(evyccjk|eppwrfh)\.(cc|com)\/pwa\.php script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91.js, requires-body=true, timeout=60, tag=91短视频
+ http-response ^https:\/\/([a-z0-9-]+\.)?(evyccjk|eppwrfh|hashichd)\.(cc|com|xyz)\/pwa\.php script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/91.js, requires-body=true, timeout=60, tag=91短视频
 
  [MITM]
- hostname = *.evyccjk.cc, *.eppwrfh.com
+ hostname = *.evyccjk.cc, *.eppwrfh.com, *.hashichd.xyz
  ***********************************/
